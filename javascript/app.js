5471d1a01f16fce0d9de994165e50b2fbbaf0493
jgheraghty <jheraghty96@gmail.com>
2018-01-18 01:11:41 +0000

    test writing to database

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,43 @@
-function level2(){
+var newUser = document.getElementById("username");
+var signUp = document.getElementById("btnSignUp");
+
+function signUp() {
+    // Get a reference to the database service
+    var database = firebase.database().ref();
+
+    var userInput = username.value;
+
+    database.ref("userName").push(userInput);
     
+    /*commented out
+    // set value of Test (or create if it does not exist)
+    database.ref("userNameB").set("Anthony");
+    // get value of Test
+    database.ref("userName").once("value").then(function (data) {
+        console.log(data.val());
+    });*/
 }
-}
+
+
+/*var mainText = document.getElementById("mainText");
+var submit = document.getElementById("submit");
+
+function Sub() {
+    // Get a reference to the database service
+    var database = firebase.database();
+
+    //Get user inputted text
+    var messageText = mainText.value;
+
+    // set value (or create if it does not exist)
+    database.ref("userNom").push(messageText);
+}*/
+
+/*// Get a reference to the database service
+var database = firebase.database();
+// set value of Test (or create if it does not exist)
+database.ref("userNom").push("Linda");
+// get value of Test
+database.ref("userNom").once("value").then(function (data) {
+    console.log(data.val());
+});*/
