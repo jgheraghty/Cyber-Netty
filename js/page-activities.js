d435d11ef0708abc7c84e57403c2545a37339926
jgheraghty <jheraghty96@gmail.com>
2018-08-23 00:01:47 +0100

    Home Page information added

--- js/page-activities.js
+++ js/page-activities.js
@@ -1,10 +1,24 @@
 (function() {
+
+    const navLoggedIn = document.getElementById('loggedIn');
+    const navJoinNetty = document.getElementById('joinNetty');
+    const navLogout = document.getElementById('btnLogout');
+
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
             console.log('Logged in');
+            document.getElementById("userGreeting").innerHTML = '<h2>Hi ' + firebaseUser.displayName + ', join Netty on a quest to discover the online world!</h2>';
 
+            navLoggedIn.classList.remove('hide');
+            navJoinNetty.classList.add('hide');
+            navLogout.addEventListener('click', e => {
+                firebase.auth().signOut();
+            });
         } else {
             console.log('Logged out');
+            navLoggedIn.classList.add('hide');
+            navJoinNetty.classList.remove('hide');
         }
     });
-})();
+
+}());
